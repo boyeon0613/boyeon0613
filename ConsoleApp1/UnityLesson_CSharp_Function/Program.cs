e9cd6693774b200bd47a753fa2bfae5b2721636a
boyeon0613 <95688274+boyeon0613@users.noreply.github.com>
2021-12-09 22:01:33 +0900

    12/9
    12/9

---
+++ ConsoleApp1/UnityLesson_CSharp_Function/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UnityLesson_CSharp_Function
+{
+    class Program
+    {
+        static bool doPrintHelloWorld = true;
+        static bool doPrintSomething = true;
+
+        static void Main(string[] args)
+
+
+        {  // 함수 호출시 함수이름 (); 형태로 호출한다.
+            if (doPrintHelloWorld == true)
+            {
+                PrintHelloWorld();
+            }
+            else
+            {
+                Console.WriteLine("do nothing");
+            }
+            // parameter : 함수 호출시 입력 변수
+            //string 안은 큰따옴표
+            string something = "unity Hola!";
+
+            if (doPrintSomething == true)
+            {
+                PrintSomoething(something);
+            }
+        }
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void PrintSomoething(string something)
+        {
+            Console.WriteLine(something);
+        }
+    }
+}
---
+++ ConsoleApp1/UnityLesson_Csharp_IF/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//모든 변수는 항상 false가 기본값 000000
+namespace UnityLesson_Csharp_IF
+{
+    class Program
+    {
+        static bool doPrint = true;
+        static void Main(string[] args)
+        {
+            if (doPrint == true)
+            {
+              Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
