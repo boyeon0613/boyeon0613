054e6b5add45bde7873c2043adbf48217d8a7d7c
boyeon0613 <95688274+boyeon0613@users.noreply.github.com>
2022-02-28 21:22:17 +0900

    2/28

--- Platformer2D/Assets/02.Scrpits/PlayerController.cs
+++ Platformer2D/Assets/02.Scrpits/PlayerController.cs
@@ -1,11 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//지면감지기능
-//1.OnCollisionEnter(Collision collision) : collsion의 GameObject의 Layer을 비교한 다음, Layer가 Ground면 Jump가 가능하도록 한다.
-//2.Raycast 선을 쏴서 닿은 오브젝트 참조
-//3.Boxcast 박스에 닿은 오브젝트 참조
-//4.Overrap 특정 범위에 특수한 오드젝트 있으면 감지
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,31 +9,169 @@ public class PlayerController : MonoBehaviour
     BoxCollider2D col;
     public float moveSpeed;
     public float jumpForce;
+    Vector2 move; // direction vector(방향 벡터), 여기서는 크기가 1 넘어가도 사용함.
 
-    PlayerState playerState;
+    int _direction;
+    int direction
+    {
+        set
+        {
+            _direction = value;
+            if (_direction < 0)
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            else if (_direction > 0)
+                transform.eulerAngles = Vector3.zero;
+        }
+        get { return _direction; }
+    }
 
-    //Detectors
+    // States    
+    public PlayerState playerState;
+    public JumpState jumpState;
+    public RunState runState;
+    public AttackState attackState;
+    public DashState dashState;
+    public DashAttackState dashAttackState;
+
+    public bool isAttacking
+    {
+        get
+        {
+            return (attackState != AttackState.Idle || 
+                    dashAttackState != DashAttackState.Idle);
+        }
+    }
+
+    // Detectors
     PlayerGroundDetector groundDetector;
 
+    // animation
+    Animator animator;
+    float animationTimeElapsed;
+    float attackTime;
+    float dashTime;
+    float dashAttackTime;
     private void Awake()
     {
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
         groundDetector = GetComponent<PlayerGroundDetector>();
+        animator = GetComponentInChildren<Animator>();
+
+        attackTime = GetAnimationTime("Attack");
+        dashTime = GetAnimationTime("Dash");
+        dashAttackTime = GetAnimationTime("DashAttack");
     }
-    private void Update()
+
+    float GetAnimationTime(string name)
     {
-        //키보드 좌우 입력 받아서 게임 오브젝트를 좌우로 움직이는 기능
+        float time = 0;
+        RuntimeAnimatorController ac = animator.runtimeAnimatorController;
+        for (int i = 0; i < ac.animationClips.Length; i++)
+        {
+            if(ac.animationClips[i].name == name)
+            {
+                time = ac.animationClips[i].length; break;
+            }
+        }
+        return time;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // 키보드 좌우 입력 받아서 게임오브젝트를 좌우로 움직이는 기능
         float h = Input.GetAxis("Horizontal");
-        rb.position += new Vector2(h * moveSpeed * Time.deltaTime, 0);
+
+        if(IsChangeDirectionPossible())
+        {
+            if (h < 0)
+                direction = -1;
+            else if (h > 0)
+                direction = 1;
+        }
+        
+        if(IsHorizontalMovePossible())
+        {
+            move.x = h;
+
+            if (groundDetector.isGrounded &&
+           jumpState == JumpState.Idle &&
+           isAttacking == false)
+            {
+                if (Mathf.Abs(h) > 0.1f)//수평 입력의 절댓값이 0보다 크면
+                {
+                    if (playerState != PlayerState.Run)//플레이어 상태가 달리고 있지 않으면
+                    {
+                        playerState = PlayerState.Run;//플레이어 상태 달리기로 바꿈
+                        runState = RunState.PrepareToRun;//달리기 상태 달리기 준비로 바꿈
+                    }
+
+                }
+                else
+                {
+                    h = 0;
+                    if (playerState != PlayerState.Idle)//플레이어가 Idle이 아니면
+                        playerState = PlayerState.Idle;//플레이어를 Idle로 만듬
+                    animator.Play("Idle");
+                }
+            }
+        }
+
+       
+        // Rigidbody.velocity 를 물리연산 주기마다 실행할 경우 
+        // 비정상적인 동작을 일으킬 가능성이 있으므로 
+        // 주기함수에서는 velocity 가 아니라 position 을 변경하는 방식으로 움직인다.
+        // velocity 를 직접 수정하는 경우는 
+        // 점프하는 순간 등 의 경우에 순간적으로 속도가 바뀌어야 할때 
+        // 또는 특정 동작에서 다른 동작으로 넘어가는 순간 속도를 재설정 해야할때 직접수정.
 
         if (playerState != PlayerState.Jump && Input.GetKeyDown(KeyCode.LeftAlt))
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             playerState = PlayerState.Jump;
+            jumpState = JumpState.PrepareToJump;
+        }
+
+        if (playerState != PlayerState.Attack && Input.GetKeyDown(KeyCode.A))
+        {
+            playerState = PlayerState.Attack;
+            attackState = AttackState.PrepareToAttack;
+        }
+
+        if(playerState != PlayerState.Dash && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            playerState = PlayerState.Dash;
+            dashState = DashState.PrepareToDash;
+        }
+
+        //Attack action
+        if(isAttacking == false && Input.GetKeyDown(KeyCode.A))
+        {
+            playerState = PlayerState.Attack;
+            attackState = AttackState.PrepareToAttack;
+
+            if(playerState == PlayerState.Dash)
+            {
+                playerState = PlayerState.DashAttack;
+                dashAttackState = DashAttackState.PrepareToDashAttack;
+            }
+        }
+        if (isAttacking == false && playerState == PlayerState.Dash && Input.GetKeyDown(KeyCode.A))
+        {
+            playerState = PlayerState.DashAttack;
+            dashAttackState = DashAttackState.PrepareToDashAttack;
         }
         UpdatePlayerState();
+    }
+    private void FixedUpdate()
+    {
+        FixedUpdateMovement();
+    }
+    private void FixedUpdateMovement()
+    {
+        Vector2 velocity = new Vector2(move.x * moveSpeed, move.y * moveSpeed);
+        rb.position += velocity * Time.fixedDeltaTime;
+        //rb.velocity = new Vector2(h * moveSpeed , 0); 
     }
 
     void UpdatePlayerState()
@@ -48,20 +181,205 @@ public class PlayerController : MonoBehaviour
             case PlayerState.Idle:
                 break;
             case PlayerState.Run:
+                UpdateRunState();
                 break;
             case PlayerState.Jump:
-                if(groundDetector.isGrounded)
-                    playerState = PlayerState.Idle;
+                UpdateJumpState();
+                break;
+            case PlayerState.Attack:
+                UpdateAttackState();
+                break;
+            case PlayerState.Dash:
+                UpdateDashState();
+                break;
+            case PlayerState.DashAttack:
+                UpdateDashAttackState();
                 break;
             default:
                 break;
         }
     }
+    private void UpdateRunState()
+    {
+        switch (runState)
+        {
+            case RunState.PrepareToRun:
+                animator.Play("Run");
+                break;
+            case RunState.Running:
+                break;
+        }
+    }
+    
+    private void UpdateJumpState()
+    {
+        switch (jumpState)
+        {
+            case JumpState.PrepareToJump:
+                animator.Play("Jump");
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                jumpState = JumpState.Jumping;
+                break;
+            case JumpState.Jumping:
+                if (groundDetector.isGrounded == false)
+                {
+                    jumpState = JumpState.InFlight;
+                }
+                break;
+            case JumpState.InFlight:
+                if (groundDetector.isGrounded)
+                {
+                    playerState = PlayerState.Idle;
+                    jumpState = JumpState.Idle;
+                    animator.Play("Idle");
+                    runState = RunState.Idle;
+                }
+                break;
+        }
+        
+    }
+    private void UpdateAttackState()
+    {
+        switch (attackState)
+        {
+            case AttackState.PrepareToAttack:
+                animator.Play("Attack");
+                attackState = AttackState.Attacking;
+                break;
+            case AttackState.Attacking:
+                if (animationTimeElapsed>attackTime)
+                {
+                    attackState = AttackState.Attacked;
+                }
+                animationTimeElapsed += Time.deltaTime;
+                break;
+            case AttackState.Attacked:
+                playerState = PlayerState.Idle;
+                attackState = AttackState.Idle;
+                animationTimeElapsed = 0;
+                animator.Play("Idle");
+                break;
+        }
+    }
+    private void UpdateDashState()
+    {
+        switch (dashState)
+        {
+            case DashState.Idle:
+                break;
+            case DashState.PrepareToDash:
+                animator.Play("Dash");
+                dashState = DashState.Dashing;
+                break;
+            case DashState.Dashing:
+                if (animationTimeElapsed < dashTime *3/4)
+                {
+                    move.x = direction*moveSpeed * 1.5f;
+                }
+                else 
+                {
+                    dashState = DashState.Dashed;
+                }
+                animationTimeElapsed += Time.deltaTime;
 
-    enum PlayerState
+                break;
+            case DashState.Dashed:
+                if(animationTimeElapsed >dashTime)
+                {
+                    playerState = PlayerState.Idle;
+                    dashState = DashState.Idle;
+                    animationTimeElapsed = 0;
+                    animator.Play("Idle");
+                }
+                else 
+                {
+                    move.x = direction * moveSpeed / 4f;
+                }
+                animationTimeElapsed += Time.deltaTime;
+                break;
+        }
+    }
+private void UpdateDashAttackState()
+    {
+        switch (dashAttackState)
+        {
+            case DashAttackState.Idle:
+                break;
+            case DashAttackState.PrepareToDashAttack:
+                break;
+            case DashAttackState.DashAttacking:
+                break;
+            case DashAttackState.DashAttacked:
+                break;
+        }
+
+    }
+    private bool IsChangeDirectionPossible()
+    {
+        bool isOK = false;
+        if (playerState == PlayerState.Idle ||
+           playerState == PlayerState.Run ||
+           playerState == PlayerState.Jump)
+        {
+            isOK = true;
+        }
+        return isOK;
+    }
+    private bool IsHorizontalMovePossible()
+    {
+        bool isOK = false;
+        if (playerState == PlayerState.Idle ||
+           playerState == PlayerState.Run ||
+           playerState == PlayerState.Jump)
+        {
+            isOK = true;
+        }
+        return isOK;
+    }
+
+    public enum PlayerState
     {
         Idle,
         Run,
         Jump,
+        Attack,
+        Dash,
+        DashAttack
+    }
+    public enum JumpState
+    {
+        Idle,
+        PrepareToJump, // Jump 에 필요한 파라미터 세팅, 애니메이션 전환 등 
+        Jumping, // Jump 물리연산을 시작하는 단계 
+        InFlight, // Jump 물리연산이 끝나고 공중에 캐릭터가 떠있는 상태
+    }
+    public enum RunState
+    {
+        Idle,
+        PrepareToRun,
+        Running
+    }
+    public enum AttackState
+    {
+        Idle, 
+        PrepareToAttack, 
+        Attacking, 
+        Attacked
+
+    }
+    public enum DashState
+    {
+        Idle,
+        PrepareToDash,
+        Dashing,
+        Dashed
+    }
+    public enum DashAttackState
+    {
+        Idle,
+        PrepareToDashAttack,
+        DashAttacking,
+        DashAttacked
     }
 }
--- Platformer2D/Assets/02.Scrpits/PlayerGroundDetector.cs
+++ Platformer2D/Assets/02.Scrpits/PlayerGroundDetector.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerGroundDetector : MonoBehaviour
-{ 
+{
     Rigidbody2D rb;
     CapsuleCollider2D col;
     Vector2 center;
     public Vector2 size;
-    //아래) 다른 클래스에서는 접근을 해야하지만 인스펙터창에서는 숨기고 싶을 때
+
+    public LayerMask groudLayer;
+    // 다른 클래스에서는 접근을 해야하지만 인스펙터창에서는 숨기고싶을때
     [HideInInspector] public bool isGrounded;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<CapsuleCollider2D>();
-        size.x = col.size.x/2;
+        size.x = col.size.x / 2;
         size.y = 0.005f;
     }
-    private void Update()
+    void Update()
     {
-        center.x = rb.position.x + col.offset.x; ;
-        center.y = rb.position.y + col.offset.y - (col.size.y/2) - size.y;
-        isGrounded = Physics2D.OverlapBox(center, size, 0);
+        center.x = rb.position.x + col.offset.x;
+        center.y = rb.position.y + col.offset.y - col.size.y / 2 - size.y * 2;
+        isGrounded = Physics2D.OverlapBox(center, size, 0, groudLayer);
     }
     private void OnDrawGizmosSelected()
     {
