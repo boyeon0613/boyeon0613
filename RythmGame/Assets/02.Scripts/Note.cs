4aa766551830ed7e640397fdb948d5e22b6415d2
boyeon0613 <95688274+boyeon0613@users.noreply.github.com>
2022-03-09 20:45:16 +0900

    3/9

---
+++ RythmGame/Assets/02.Scripts/Note.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public KeyCode keyCode;
+    Transform tr;
+    public float noteSpeed;
+    private void Awake()
+    {
+        tr = transform;
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        tr.Translate(Vector2.down * noteSpeed * Time.deltaTime);
+    }
+    public void Hit(HitType type)
+    {
+        switch (type)
+        {
+            case HitType.Bad:
+                break;
+            case HitType.Miss:
+                break;
+            case HitType.Good:
+                break;
+            case HitType.Great:
+                break;
+            case HitType.Cool:
+                break;
+            default:
+                break;
+        }
+    }  
+}
+public enum HitType
+{
+    Bad,
+    Miss,
+    Good,
+    Great,
+    Cool
+}
--- RythmGame/Assets/02.Scripts/NotesMaker.cs
+++ RythmGame/Assets/02.Scripts/NotesMaker.cs
@@ -27,9 +27,10 @@ public class NotesMaker : MonoBehaviour
     }
     private void StartRecording()
     {
-        SongData songData = new SongData();
+        songData = new SongData();
         songData.videoName = vp.clip.name;
         vp.Play();
+        Debug.Log("Start recording");
     }
     public void Recording()
     {
@@ -56,13 +57,31 @@ public class NotesMaker : MonoBehaviour
     private void CreateNoteData(KeyCode keyCode)
     {
         NoteData noteData = new NoteData();
-        float roundedTime = (float)Math.Round(vp.time, 2);
-        noteData.time = (float)vp.time;
+        //float roundedTime = (float)Math.Round(vp.time, 2);
+
+        float tmpTime = (float)vp.time * 1000;
+        if(tmpTime % 10 < 5)
+        {
+            tmpTime = tmpTime /= 10;
+        }
+        else
+        {
+            tmpTime /= 10;
+            tmpTime++;
+        }
+
+        int tmpTimeInt = (int)tmpTime;
+        float roundedTime = (float)tmpTime/100;
+
+        noteData.time = roundedTime;
         noteData.keyCode = keyCode;
-        songData.notes.Add(noteData); 
+        songData.notes.Add(noteData);
+        Debug.Log(songData);
+
     }
     private void SaveSongData()
     {
+        Debug.Log("Save SongData2");
         //panel만 띄우고 선택지 디렉토리 문자열 반환(저장하지 않음)
         string dir = EditorUtility.SaveFilePanel("저장할 곳을 지정하세요", "", $"{songData.videoName}", "json");
         //실제 song data를 json 포맷으로 저장
