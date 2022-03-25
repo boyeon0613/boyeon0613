07a246ee32f9c7fbfc0a3a3f2976f7ef904d29cf
boyeon0613 <95688274+boyeon0613@users.noreply.github.com>
2022-03-25 21:54:48 +0900

    3/25

--- Tower Defence/Assets/02.Scripts/Towers/Tower.cs
+++ Tower Defence/Assets/02.Scripts/Towers/Tower.cs
@@ -10,7 +10,9 @@ public class Tower : MonoBehaviour
     public float detectRange;
 
     public Transform turretRotationPoint;
+    public Transform target;
     Transform tr;
+
     private void Awake()
     {
         tr = transform;
@@ -21,14 +23,19 @@ public class Tower : MonoBehaviour
         ObjectPool.ReturnToPool(gameObject);
     }
 
-    private void Update()
+    public virtual void Update()
     {
         Collider[] cols = Physics.OverlapSphere(tr.position, detectRange, enemyLayer);
         
         if(cols.Length>0)
         {
             cols.OrderBy(x => (x.transform.position - WayPoints.points.Last().transform.position));
+            target = cols[0].transform;
             turretRotationPoint.LookAt(cols[0]. transform);
+        }
+        else
+        {
+            target = null;
         }
     }
 
