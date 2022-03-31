ecf2eaf3ab0115781dd6ba4e0db18f898f6ff3e5
boyeon0613 <95688274+boyeon0613@users.noreply.github.com>
2022-03-31 21:57:03 +0900

    3/31

---
+++ Runner/Assets/02.Scripts/MapSpawner.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class MapSpawner: MonoBehaviour
+{
+    public static MapSpawner instance;
+    public float mapMoveSpeed = 5f;  //맵의 이동속도
+    
+    [SerializeField] List<Transform> mapTiles; // 생성할 맵타일 프리팹 목록
+    [SerializeField] Transform map; //맵타일의 부모 Transform
+    //Linked List : 리스트의 요소가 다른 요소를 멤버로 가지는 형태의 리스트
+    public LinkedList<Transform> spawnedMapTiles = new LinkedList<Transform>(); //생성될 맵파일 목록
+
+    //==================================================
+    //******************Private Method*******************
+    //==================================================
+    /// <summary>
+    /// 첫번째 맵타일을 삭제하고 
+    /// Linked List의 가장 마지막에 새로 맵타일을 생성한다.
+    /// </summary>
+    public static void RemoveFirstAndSpawnNew()
+    {
+        Destroy(instance.spawnedMapTiles.First().gameObject); //첫번째 맵타일 게임오브젝트 파괴
+        instance.spawnedMapTiles.RemoveFirst(); //첫번째 맵타일을 리스트에서 제거
+        instance.SpawnRandomMapTile(); //랜덤 맵타일 제일 마지막에 생성
+    }
+    /// <summary>
+    /// 싱글톤 + 
+    /// 생성된 맵 리스트에 미리 생성된 맵타일들을 추가.
+    /// </summary>
+    private void Awake()
+    {
+        instance = this;
+        for(int i = 0; i < map.childCount; i++)
+            spawnedMapTiles.AddLast(map.GetChild(i));
+    }
+
+    private void FixedUpdate()
+    {
+        MoveMapTiles();
+    }
+    /// <summary>
+    /// 모든 맵타일 이동
+    /// </summary>
+    private void MoveMapTiles()
+    {
+        foreach (var mapTile in spawnedMapTiles)
+        {
+            mapTile.position += Vector3.back * mapMoveSpeed * Time.fixedDeltaTime;
+        }
+    }
+    /// <summary>
+    /// 랜덤한 맵타일을
+    /// 현재 존재하는 맵타일 중에 가장 마지막 맵타일의 다음 위치에 생성한다.
+    /// 다음 위치 = 가장 마지막 맵타일 위치 + (가장 마지막 맵타일 길이 / 2) + (새로 생성한 맵타일 길이/2)
+    /// </summary>
+    private void SpawnRandomMapTile()
+    {
+        int randomIndex = Random.Range(0, mapTiles.Count);
+        Transform lastMapTile = spawnedMapTiles.Last(); //마지막 맵타일
+        float lastMapTileLength = lastMapTile.GetComponent<BoxCollider>().size.z; //마지막 맵타일 길이
+        float currentMapTileLength = mapTiles[randomIndex].GetComponent<BoxCollider>().size.z; //새로 생성할 맵타일 길이
+        Vector3 spawnPos = lastMapTile.position + Vector3.forward * ((lastMapTileLength + currentMapTileLength) / 2); //새로 생성할 위치
+        Transform spawnedMapTile = Instantiate(mapTiles[randomIndex],
+                             spawnPos,
+                             Quaternion.identity);
+        spawnedMapTiles.AddLast(spawnedMapTile); //linked list에 등록
+    }
+
+
+}
+
---
+++ Runner/Assets/02.Scripts/PlayerStateMachine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    public State state;
+    public KeyCode keyCode;
+    public Animator animator;
+
+    private void Awake()
+    {
+        animator= GetComponent<Animator>();
+    }
+    //머신 동작 끝났는지 체크
+    public bool isFinished
+    {
+        get
+        {
+            return state == State.Finish;
+        }
+    }
+
+    /// <summary>
+    /// 머신 동작 가능 여부 체크
+    /// </summary>
+    /// <returns></returns>
+    public virtual bool IsExecuteOK()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// 머신 동작
+    /// </summary>
+    public virtual void Execute()
+    {
+        state = State.Prepare;
+    }
+    /// <summary>
+    /// 머신 동작 업데이트
+    /// </summary>
+    public virtual void UpdateState()
+    {
+
+    }
+
+    /// <summary>
+    /// 머신 강제종료
+    /// </summary>
+    public virtual void Stop()
+    {
+        state = State.Idle;
+    }
+
+    public enum State
+    {
+        Idle,
+        Prepare,
+        Casting,
+        OnAction,
+        Finish
+    }
+
+
+}
+
